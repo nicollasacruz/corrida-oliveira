4377ffa115741bdfe5ec85f87e3c182aaefa72aa
Nicollas Cruz <nicollasacruz@gmail.com>
2025-03-13 14:47:00 +0000

    initial commit

---
+++ resources/js/Pages/Events/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import GuestLayout from '@/Layouts/GuestLayout'
+  import { Head } from '@inertiajs/vue3'
+
+  defineProps({
+    events: {
+      type: Array,
+    }
+  })
+</script>
+
+<template>
+  <GuestLayout>
+    <Head title="Index" />
+
+  </GuestLayout>
+</template>
