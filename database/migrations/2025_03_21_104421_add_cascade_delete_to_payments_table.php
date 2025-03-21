266a9fa49d8f836836be326e4f0926a79c2a054c
Nicollas Cruz <nicollasacruz@gmail.com>
2025-03-21 10:51:24 +0000

    Ajustes participantes delete

--- database/migrations/2025_03_21_104421_add_cascade_delete_to_payments_table.php
+++ database/migrations/2025_03_21_104421_add_cascade_delete_to_payments_table.php
@@ -6,29 +6,45 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->foreignId('participant_id')->change()->constrained()->cascadeOnDelete();
+            // Remove FK existente
+            $table->dropForeign(['participant_id']);
+
+            // Adiciona FK com onDelete('cascade')
+            $table->foreign('participant_id')
+                ->references('id')
+                ->on('participants')
+                ->onDelete('cascade');
         });
+
         Schema::table('runner_kits', function (Blueprint $table) {
-            $table->foreignId('participant_id')->change()->constrained()->cascadeOnDelete();
+            // Remove FK existente
+            $table->dropForeign(['participant_id']);
+
+            // Adiciona FK com onDelete('cascade')
+            $table->foreign('participant_id')
+                ->references('id')
+                ->on('participants')
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->foreignId('participant_id')->change()->constrained();
+            $table->dropForeign(['participant_id']);
+            $table->foreign('participant_id')
+                ->references('id')
+                ->on('participants');
         });
+
         Schema::table('runner_kits', function (Blueprint $table) {
-            $table->foreignId('participant_id')->change()->constrained();
+            $table->dropForeign(['participant_id']);
+            $table->foreign('participant_id')
+                ->references('id')
+                ->on('participants');
         });
     }
 };
