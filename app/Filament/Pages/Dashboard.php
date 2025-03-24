01d643d669604867a8e65fa8a48c5d8feb113b60
Nicollas Cruz <nicollasacruz@gmail.com>
2025-03-24 13:14:33 +0000

    Criado dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -13,8 +13,6 @@ class Dashboard extends BaseDashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'filament.pages.dashboard';
-
     protected function getHeaderWidgets(): array
     {
         return [
