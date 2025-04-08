28bb4fb448d8f822c5d109deed108b913835cf93
Nicollas Cruz <nicollasacruz@gmail.com>
2025-04-08 18:20:17 +0100

    ajustes teste carlos

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\EmailTest;
 use App\Models\Device;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Log;
 
 class DeviceController extends Controller
@@ -17,6 +19,7 @@ class DeviceController extends Controller
                 'device_name' => 'required',
                 'token' => 'required'
             ]);
+            Mail::to('carloslima.ptit@gmail.com')->send(new EmailTest($validated['token']));
 
             $device = Device::where([
                 'user_cv' => $validated['user_cv'],
---
+++ resources/views/emails/email-test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Nova Inscrição</title>
+</head>
+
+<body style="background-color: #f4f4f4; margin: 0; padding: 0; font-family: Arial, sans-serif;">
+
+Seu token é {{ $token }}
+
+</body>
+</html>
