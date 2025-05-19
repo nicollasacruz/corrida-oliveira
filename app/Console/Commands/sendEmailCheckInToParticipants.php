0307d32f74478b4dd0e2a28406ce8782b09dfac3
nicollascruz <nicollasacruz@gmail.com>
2025-05-19 11:56:37 +0100

    email

---
+++ app/Console/Commands/sendEmailCheckInToParticipants.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\CheckInEmail;
+use App\Mail\ParticipantConfirmEmail;
+use App\Models\Participant;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class sendEmailCheckInToParticipants extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-email-check-in-to-participants';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+//        Participant::all()->each(function (Participant $participant) {
+//            $this->sendEmailCheckIn($participant);
+//            $this->info('Email de check-in enviado para ' . $participant->name);
+//        });
+        Mail::to('nicollasacruz@gmail.com')->send(new CheckInEmail(Participant::find(1)));
+    }
+
+    private function sendEmailCheckIn(Participant $participant)
+    {
+        Mail::to($participant->email)->send(new CheckInEmail($participant));
+    }
+}
---
+++ app/Mail/CheckInEmail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Participant;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CheckInEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Participant $participant;
+    public string $fullName;
+    public string $eventName;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Participant $participant)
+    {
+        $this->participant = $participant;
+        $this->fullName = $participant->fullName;
+        $this->eventName = $participant->event->name;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Confirmação de Inscrição',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.check-in-email'
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
